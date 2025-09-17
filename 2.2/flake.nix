3160426c9f569fb1b87c950468d96baed0ab047c
User1012007 <rojasbadilloe@gmail.com>
2025-09-17 08:40:24 -0600

    Tarea 2.2 de progra ej1 en C con su flake

---
+++ 2.2/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Minimal flake to build and run a C program";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in {
+      packages = forEachSystem ({ pkgs }: {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "p48e1N_6A_23";
+          version = "1.0";
+
+          src = ./.;
+          buildInputs = [ pkgs.gcc ];
+          buildPhase = ''
+            gcc p48e1N_6A_23.c -o p48e1N_6A_23
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp p48e1N_6A_23 $out/bin/
+          '';
+        };
+      });
+
+      apps = forEachSystem ({ pkgs }: {
+        default = {
+          type = "app";
+          program = "${self.packages.${pkgs.system}.default}/bin/p48e1N_6A_23";
+        };
+      });
+    };
+}
---
+++ 2.2/p48e1N_6A_23.c
@@ -0,0 +1,26 @@
+/*Nombre del Programa: Plantilla base_V1
+Elaboró: MLMA
+No Lista: NL
+Fecha: Hoy
+No. Versión: 1
+Descripción:Muestra la Plantilla base de un
+programa en Lenguaje C */
+#include <math.h>  // Incluye cabecera en el archivo
+#include <stdio.h> // Incluye cabecera en el archivo
+
+int main(int argc, char *argv[]) {
+  int v0, t, d0; // declaracion de variables locales enteros
+  float a;       // declaracion de variables locales flotantes
+  printf("Ingrese velocidad inicial [m/s]: "); // Imprime pedir datos
+  scanf("%d", &v0);               // Almacena en variable el dato ingresado
+  printf("Ingrese tiempo [s]: "); // Imprime pedir datos
+  scanf("%d", &t);                // Almacena en variable el dato ingresado
+  printf("Ingrese desplazamiento inicial [m]: "); // Imprime pedir datos
+  scanf("%d", &d0); // Almacena en variable el dato ingresado
+  printf("Ingrese aceleracion [m/s^2]: "); // Imprime pedir datos
+  scanf("%f", &a); // Almacena en variable el dato ingresado
+  float df =
+      d0 + (v0 * t) + (a * t * t / 2); // Hace calculo con los datos ingresadoa
+  printf("El desplazamiento final es: %.2f\n", df); // Imprime calculo final
+  return 0;
+}
