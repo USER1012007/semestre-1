3a1006b491eea79d89c5dde87d442ea8db72910e
User1012007 <rojasbadilloe@gmail.com>
2025-10-29 20:04:14 -0600

    examen-3: add code of the 3rd unit exam

---
+++ examen-3/ej1/main.c
@@ -0,0 +1,46 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  int op, n;
+
+  printf("1) serie de fibonacci\n2) ecuaciones\ningresse tu opcion: ");
+  scanf("%d", &op);
+
+  switch (op) {
+  case 1:
+    int tmp = 1;
+    printf("Ingrese cantidad de elementos:\n");
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++) {
+      printf("%d, ", tmp);
+      tmp = tmp * -1 * (i + 1) / (i + 2);
+    }
+    break;
+
+  case 2:
+    float y;
+    printf("Ingrese cantidad de ecuaciones:\n");
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++) {
+      printf("Ingrese valor de y:\n");
+      scanf("%f", &y);
+      if (y > 60 || y <= 0) {
+        y = 0;
+      } else if (y > 0 && y <= 15) {
+        y = pow(y, 2) + 15;
+      } else if (y > 15 && y <= 30) {
+        y = pow(y, 3) - pow(y, 2) + 12;
+      } else if (y > 30 && y <= 60) {
+        y = 4 * pow(y, 3) / pow(y, 2) + 8;
+      }
+      printf("y = %.2f\n", y);
+    }
+    break;
+
+  default:
+    printf("ingrese opcion correcta\n");
+  }
+
+  return 0;
+}
